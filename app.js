1b1cb3cc7e8db0dfa9019fa2f1a83fbe6b533e7d
Gon Espeche <39110211+gonpeche@users.noreply.github.com>
2018-07-04 15:54:26 -0300

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,15 @@ var budgetController = (function () {
 // UI CONTROLLER
 var UIController = (function () {
 
-    // some code
+    return {
+        getInput: function () {
+            return {
+                type: document.querySelector('.add__type').value, // Will be either inc or exp
+                description: document.querySelector('.add__description').value,
+                value: document.querySelector('.add__value').value
+            };
+        }
+    }
 
 })();
 
@@ -21,10 +29,28 @@ var UIController = (function () {
 // GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
   
-    document.querySelector('.add__btn').addEventListener('click', function() {
+    var ctrlAddItem = function () { 
 
-        // T
+        // 1. Get the field input data
 
+        // 2. Add the item to the budget controller
+
+        // 3. Add the new item to the UI
+
+        // 4. Calculate the budget
+
+        // 5. Display the budget on the UI
+
+        console.log("funciona!")
+    }
+
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function (event) { // event or "e" is also common to use
+
+        if (event.keyCode === 13 || event.which === 13) { // which is for older browsers that do not have the keyCode
+            ctrlAddItem();
+        }
     })
 
 })(budgetController, UIController); // Here they are linked to budgetCtrl and UICtrl.
