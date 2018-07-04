fb6f177948138067be45e3baed16f0317e1246d9
Gon Espeche <39110211+gonpeche@users.noreply.github.com>
2018-07-04 17:28:16 -0300

    Update app.js

--- app.js
+++ app.js
@@ -13,8 +13,20 @@ var budgetController = (function () {
         this.value = value;
     };
 
-})();
 
+    var data = {
+        allItems: {
+            exp: [],
+            inc: []
+        },
+        totals: {
+            exp: 0,
+            inc: 0
+        }
+
+    }
+
+})();
 
 
 
