50845ee5ff55a64a711e5256f126e4154b102185
Gon Espeche <39110211+gonpeche@users.noreply.github.com>
2018-07-06 12:26:26 -0300

    Clear input fields

--- app.js
+++ app.js
@@ -109,12 +109,26 @@ var UIController = (function () {
             document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
         },
 
+        clearFields: function() { // clears the fields input
+            var fields, fieldsArr;
+            fields = document.querySelectorAll(DOMstrings.inputDescription +  ', ' + DOMstrings.inputValue); // returns a list that we have to convert to an array. We use the array method called SLICE, that returns a copy of the array. The trick is to send a list "as" an array.
+
+            fieldsArr = Array.prototype.slice.call(fields); 
+
+            fieldsArr.forEach(function(current, index, array) {
+                current.value = "";
+            });
+
+            fieldsArr[0].focus();
+        },
+
         getDOMstrings: function () {
             return DOMstrings;
         },
     };
 
 })();
+
 
 
 
@@ -147,9 +161,13 @@ var controller = (function(budgetCtrl, UICtrl) {
 
         // 3. Add the new item to the UI
         UICtrl.addListItem(newItem, input.type);
-        // 4. Calculate the budget
 
-        // 5. Display the budget on the UI
+        // 4. Clear the fields
+        UICtrl.clearFields();
+
+        // 5. Calculate the budget
+
+        // 6. Display the budget on the UI
 
     };
 
@@ -164,6 +182,5 @@ var controller = (function(budgetCtrl, UICtrl) {
 
 
 controller.init();
-
 
 
